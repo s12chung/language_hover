61664565d42637a874b1074ae30338a495ef971d
s12chung <steve.chung7@gmail.com>
2014-02-08 21:41:41 -0500

    fix editing

--- app/presenters/note_presenter.rb
+++ app/presenters/note_presenter.rb
@@ -3,7 +3,7 @@ module NotePresenter
     text.split(/(?:\r\n){2}/).map do |paragraph|
       content_tag :p do
         paragraph.split(/\r\n/).map do |sentence|
-          text, romanization = sentence.split("==")
+          text, romanization = sentence.split(" == ")
           content_tag :span, text, data: { behavior: "tipsy text_to_speech",
                                            tipsy: romanization,
                                             text_to_speech: text.sub(/\!/, "")}
